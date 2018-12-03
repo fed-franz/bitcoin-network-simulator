c86b6e34b2db5ccbdafe899fcdea4058d5d3a5d9
FedFranz <frz.dev@gmx.com>
2018-12-03 17:19:16 +0100

    DNS: add dynamic BIND setting via mounted volume

--- bind-dock/start-dns.sh
+++ bind-dock/start-dns.sh
@@ -2,8 +2,14 @@
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
 echo "alias dnslog='tcpdump -i eth0 -vvv -s 0 -l -n port 53 &'" >> ~/.bashrc
 
+# Copy files from mounted directory to bind directory
+yes | cp -rf /root/mnt/* /etc/bind/
+
+# Starts log service and BIND9 with log enables
 service rsyslog start
 /etc/init.d/bind9 start
 rndc querylog
-chmod -R 0777 /etc/bind/zones/
+chmod -R 0755 /etc/bind/zones/
+
+# Keeps the container running
 tail -f /dev/null
